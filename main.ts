14f51115e2a79f5428d3641e25ba930cabcb1430
karlr <andrew.mikedaniels@gmail.com>
2024-08-22 05:57:04 -0700

    init commit

--- main.ts
+++ main.ts
@@ -1,1 +1,15 @@
- 
+
+const SKIN_SHEDDING_ELEGY = assets.song`skinSheddingElegy`
+const PEWTER_CITY = assets.song`pewterCity`
+const FUNERAL_MARCH = assets.song`funeralMarch`
+const DISCOVERY_FANFARE = assets.song`discoveryFanfare`
+const SUPER_METROID_COUNTDOWN = assets.song`superMetroidCountdown`
+const HYPER_THEME = assets.song`hyperTheme`
+const WHAT = assets.song`What`
+const POWERUP = assets.song`linksAwakeningPowerup`
+const POWERUP_GET = assets.song`linksAwakeningPowerupGet`
+const ELEVATOR = assets.song`elevator`
+const DOPIC_TOWN_001 = assets.song`dopicTown1`
+const DOPIC_TOWN_002 = assets.song`dopicTown2`
+const DOPIC_TOWN_003 = assets.song`dopicTown3`
+const DOPIC_TOWN_004 = assets.song`dopicTown4`
